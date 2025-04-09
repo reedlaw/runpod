8cc90e3adf2f35b94461de297c065dbf34f57fa8
Reed Law <reedlaw@gmail.com>
2025-04-09 13:52:56 -0400

    spacy

--- rp_handler.py
+++ rp_handler.py
@@ -1,20 +1,15 @@
 import runpod
 import time
 import spacy
-from spacy.matcher import Matcher
 
 def handler(event):
     input = event['input']
-    nlp = spacy.load("en_core_web_sm")
-    matcher = Matcher(nlp.vocab)
-    # Add match ID "HelloWorld" with no callback and one pattern
-    pattern = [{"LOWER": "hello"}, {"IS_PUNCT": True}, {"LOWER": "world"}]
-    matcher.add("HelloWorld", [pattern])
-
-    doc = nlp("Hello, world! Hello world!")
-    matches = matcher(doc)
-
-    return matches
+    nlp = spacy.load("en_core_web_lg")
+    doc = nlp(input['prompt'])
+    ents = []
+    for ent in doc_en.ents:
+        ents.append({"text": ent.text, "label_": ent.label_})
+    return ents
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler})
